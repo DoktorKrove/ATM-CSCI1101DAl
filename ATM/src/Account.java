f03f7b6e738e01b7d07e976b1874afdebfb6a05c
Leif Lang <Leif Lang>
2016-03-21 19:46:40 -0300

    Added all basic methods

--- ATM/src/Account.java
+++ ATM/src/Account.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
 
 public class Account {
 
+	String name, userID;
+	//User holder = new User(); - Requires the user class to be done!
+	//ArrayList<Transaction> transactions = new ArrayList<Transaction>  - Requires the Transaction class to be done!
+
+	/*
+	public Account(String name, User holder, Bank theBank)
+	{
+		this.name = name;
+		this.holder = holder;
+	}
+	*/
+	public String getUserID() {
+		return userID;
+	}
+	
+	/*
+	public void addTransaction(double amount)
+	{
+	}
+	*/
+	/*
+	public void addTransaction(double amount, String memo)
+	{
+	}
+	*/
+	/*
+	public double getBalence()
+	{
+	}
+	*/
+	/*
+	public String getSummaryLine()
+	{ 
+	}
+	*/
+	/*
+	public void printTransactionHistory()
+	{
+	} 
+	*/
 }
