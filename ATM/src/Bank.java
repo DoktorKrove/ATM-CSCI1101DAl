ab2964ab5da5155143711d23e5c8902deb60c933
Joel <Joelfong1@gmail.com>
2016-04-11 01:02:14 -0300

    Changed to simplified gimpy version.

--- ATM/src/Bank.java
+++ ATM/src/Bank.java
@@ -1,43 +1,31 @@
 import java.util.ArrayList;
 
-//Aggregates from account and user
+
 public class Bank
 {
 	private String name;
 	private ArrayList<User> users = new ArrayList<User>();
-	private ArrayList<Account> accounts = new ArrayList<Account>();
-	
+   	
 	//constructor
 	public Bank(String name)
 	{
 		this.name = name;
 		users = new ArrayList<User>();
-		accounts = new ArrayList<Account>();
 	}
-	
-	public String getNewUserID()
-	{
-	}
-	
-	public String getNewAccountID()
-	{
-	}
-	
-	public User addUser(String firstName, String lastName, String pin)
-	{
-	}
-
-	public void addAccount(Account newAccount)
-	{
-		this.accounts.add(newAccount);
-	}
-	
-	public User userLogin(String userID, String pin)
-	{
-	}
-	
-	public String getName()
+   
+   public String getName()
 	{
 		return this.name;
 	}
+	
+   
+   public boolean findUser(User user)
+   {
+      if (users.contains(user))
+         return true;
+      else
+         return false;
+   }
+	
+	
 }
